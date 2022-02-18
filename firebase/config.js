abeb39d0b92d5d33ad1a7cca9531b6939dff6214
Gedin <kahlil.gedin@gmail.com>
2022-02-18 07:15:44 -0800

    Add files via upload

---
+++ firebase/config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhMxwmJwAyRcCJEdZM3v_z4NzLq4FwP1A",
+  authDomain: "firegram-52724.firebaseapp.com",
+  projectId: "firegram-52724",
+  storageBucket: "firegram-52724.appspot.com",
+  messagingSenderId: "788852885812",
+  appId: "1:788852885812:web:3635900f1dc16815927d61"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Initialize Project Storage
+const projectStorage = firebase.storage();
+
+//INITIALIZE FIRESTRORE 
+const projectFirestore = firebase.firestore();
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export {projectStorage, projectFirestore, timestamp};
